9a033bd69b005e7868875635680e00e9c00a1dd3
Andreea Manuela  Hodor <andreea.hodor@gmail.com>
2026-01-25 00:01:41 +0100

    added comments

--- Rush01/input_to_array.c
+++ Rush01/input_to_array.c
@@ -10,27 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	initialize(char *str, int *attr, int arr[4][4])
+void	initialize(char *str, int *attr, int array[4][4])
 {
+	//if column reached 4
 	if (attr[1] == 4)
 	{
+		//reset index to zero
 		attr[1] = 0;
+		//move to next row
 		attr[2] += 1;
 	}
+	//convert to int
 	arr[attr[2]][attr[1]] = str[attr[0]] - 48;
+	//move to next column
 	attr[1]++;
+	//move to next char in str
 	attr[0]++;
 }
 
 int	input_to_array(char *str, int *attr, int array[4][4])
 {
+	//loop until end of string
 	while (str[attr[0]] != '\0')
 	{
+		//if char is '1' and '4'
 		if (str[attr][0] >= '1' && str[attr[0]] <= '4')
+			//add to array
 			initialize(str, attr, arr);
+		//if space
 		else if (str[attr[0]] == ' ')
-			
+			//skip
+			attr[0]++,
+		else
+		{
+			//error for invalid char
+			write(1, "Error", 5);
+			return (1);
+		}
 	}
+	//if input too long
+	if (attr[0] >=32)
+	{
+		//error
+		write(1, "Error", 5);
+		return (1);
+	}
+	//success
+	return (0);
 }
--- Rush01/main.c
+++ Rush01/main.c
@@ -12,10 +12,31 @@
 
 #include <unistd.h>
 
+//Functions prototype
+int	input_to_array(char *str, int *attr, int array[4][4]);
+
+//Int main
 int	main(int argc, char **argv)
 {
+	//declare variables
 	int	i;
 	int	array[4][4];
+	int	attr[3];
 
+	//check if exactly one argument is passed
+	if (argc == 2)
+	{
+		i = -1;
+		while (i++ < 3)
+			attr[i] = 0;
+		//set bottom right corner to -1 for placeholder
+		array[3][3] = -1;
+		if (input_to_array(argv[1], attr, array) == 1)
+			return (0);
+		solve(array);
+	}
+	else
+		//print error
+		write(1, "Error", 5);
 	return (0);
 }
